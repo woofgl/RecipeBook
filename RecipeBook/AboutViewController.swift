7fb63893031689f3cd2f4815bbfc7f20fbcac31f
woofgl <woofgl@gmail.com>
2015-12-24 15:33:15 +0800

    . create tab bar controller and web view

---
+++ RecipeBook/AboutViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AboutViewController.swift
+//  RecipeBook
+//
+//  Created by woofgl on 15/12/24.
+//  Copyright © 2015年 woofgl. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet var webView: UIWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        // Add code to load web content in UIWebView
+
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: "https://github.com/")!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
